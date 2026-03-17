ec41461b5011c9765454f1796d68954c02be2299
thuthao211 <030239230218@st.buh.edu.vn>
2026-03-17 09:25:49 +0700

    cap nhat ten

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/ten',function(){
+Route::get('/thuthao',function(){
 return 'Thuthao211';
 });
