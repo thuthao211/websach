05b347ff3ce9c29cf84f2338ac187f84f42c0854
bachhop08 <030239230061@st.buh.edu.vn>
2026-03-17 19:48:39 +0700

    cap nhap ten

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/thuthao',function(){
-return 'Thuthao211';
+Route::get('/bachhop',function(){
+return 'BachHop';
 });
