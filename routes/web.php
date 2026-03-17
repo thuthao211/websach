950762fe1db351247f693ed0ba44b7d2595afc06
ltphuonganh <030239230007@st.buh.edu.vn>
2026-03-17 19:48:17 +0700

    completed 7.3 _ Phuong Anh

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/thuthao',function(){
 return 'Thuthao211';
+});
+
+
+Route::get('/top-budget', function () {
+    $movies = DB::table('movie')
+                ->orderBy('budget', 'desc')
+                ->limit(10)
+                ->get();
+
+    return view('top_budget', ['movies' => $movies]);
 });
