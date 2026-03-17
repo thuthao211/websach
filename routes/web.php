ccd7f585c64212f1d98e6c6180babbbd0c39ed73
bachhop08 <030239230061@st.buh.edu.vn>
2026-03-17 21:55:27 +0700

    cau 7.1 7.2

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/bachhop',function(){
-return 'BachHop';
+    return 'Bach Hop';
+});
+
+Route::get('/theloai', function () {
+    $data = DB::select("SELECT genre_name, genre_name_vn FROM genre");
+    return view('theloai', ['data' => $data]);
+});
+
+Route::get('/top10phimvote', function () {
+    $data = DB::select("
+        SELECT movie_name, release_date, vote_average 
+        FROM movie 
+        ORDER BY vote_average DESC 
+        LIMIT 10
+    ");
+    return view('top10phimvote', ['data' => $data]);
 });
