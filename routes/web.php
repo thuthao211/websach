15d9161015021fab1ca6ddee605205adb3d25726
Thu Hoai <030239230057@st.buh.edu.vn>
2026-03-18 14:55:46 +0700

    Thu Hoai

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController; 
 
-Route::get('/thuhoai', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/thuthao',function(){
-return 'Thuthao211';
+
+Route::get('/thuhoai', function () {
+    return ('Thu Hoai');
 });
+Route::get('/movies/action', [MovieController::class, 'actionMovies']);
+
