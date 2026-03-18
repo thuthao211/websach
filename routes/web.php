3260e234102d6bb8475492a9b06be1bf9915c9ac
thuthao211 <030239230218@st.buh.edu.vn>
2026-03-18 18:13:08 +0700

    chỉnh

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-use App\Http\Controllers\MovieController; 
-=======
-use Illuminate\Support\Facades\DB;
->>>>>>> remotes/origin/thachthao
-=======
-use Illuminate\Support\Facades\DB;
->>>>>>> remotes/origin/phuonganh
 
-// Route mặc định
 Route::get('/', function () {
     return view('welcome');
 });
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 Route::get('/thuthao',function(){
-<<<<<<< HEAD
     return 'Thuthao211';
 });
 
@@ -34,70 +19,7 @@ Route::get('/panhle', function () {
 Route::get('/ThachThao', function () {
     return 'Thach Thao';
 });
-=======
 Route::get('/thuhoai', function () {
     return ('Thu Hoai');
 });
-Route::get('/movies/action', [MovieController::class, 'actionMovies']);
 
->>>>>>> remotes/origin/thuhoai
-=======
-// Route của Thư Thảo (Yêu cầu mục 3)
-Route::get('/thuthao', function () {
-    return 'Thuthao211';
-});
-
-Route::get('/thachthao', function () {
-
-    $movies = DB::table('movie')
-                ->where('runtime', '>', 120)
-                ->limit(10)
-                ->get();
-
-    return view('thachthao_74', ['data' => $movies]);
-});
->>>>>>> remotes/origin/thachthao
-=======
-return 'Thuthao211';
-});
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-Route::get('/top-budget', function () {
-    $movies = DB::table('movie')
-                ->orderBy('budget', 'desc')
-                ->limit(10)
-                ->get();
-
-    return view('top_budget', ['movies' => $movies]);
-});
->>>>>>> remotes/origin/phuonganh
-=======
-Route::get('/canada', 'App\Http\Controllers\MovieController@phimCanada');
->>>>>>> remotes/origin/nhuthao
-=======
-use Illuminate\Support\Facades\DB;
-
-Route::get('/bachhop',function(){
-    return 'Bach Hop';
-});
-
-Route::get('/theloai', function () {
-    $data = DB::select("SELECT genre_name, genre_name_vn FROM genre");
-    return view('theloai', ['data' => $data]);
-});
-
-Route::get('/top10phimvote', function () {
-    $data = DB::select("
-        SELECT movie_name, release_date, vote_average 
-        FROM movie 
-        ORDER BY vote_average DESC 
-        LIMIT 10
-    ");
-    return view('top10phimvote', ['data' => $data]);
-});
->>>>>>> remotes/origin/bachhop
-=======
-Route::get('/cau77', "App\Http\Controllers\MovieController@layThongTinPhim");
->>>>>>> remotes/origin/thuthao
