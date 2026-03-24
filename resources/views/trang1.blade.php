aab83e7fbc7bc01a19d9846f9ec5b4b6c91831ba
thuthao211 <030239230218@st.buh.edu.vn>
2026-03-24 14:38:33 +0700

    Revert "Nhuthao"

--- resources/views/trang1.blade.php
+++
@@ -1,9 +0,0 @@
-@extends("main")
-@section("title","Trang nội dung 1")
-@section("content")
-<div class="bg-primary m-5" 
-style='height:300px;display:flex;align-items:center; justify
-content:center;color:white'> 
-NỘI DUNG CHÍNH CỦA TRANG 1
-</div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\MovieController;
-<<<<<<< HEAD
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,42 +26,6 @@ Route::get('/bachhop', function () {
     return 'Bach Hop';
 });
 
-=======
-use App\Http\Controllers\ViduLayoutController;
-
-// Trang chủ
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Route từng thành viên
-Route::get('/thuthao', function () {
-    return 'Thuthao211';
-});
-
-Route::get('/phuonganh', function () {
-    return 'Phuong Anh';
-});
-
-Route::get('/panhle', function () {
-    return 'Panh Le';
-});
-
-Route::get('/thachthao', function () {
-    return 'Thach Thao';
-});
-
-Route::get('/thuhoai', function () {
-    return 'Thu Hoai';
-});
-
-Route::get('/bachhop', function () {
-    return 'Bach Hop';
-});
-
-
-// 7.4: runtime > 120
->>>>>>> 1e69824c31fcd6130eb5b9798a26f73d86391ef0
 Route::get('/movies/runtime', function () {
     $movies = DB::table('movie')
         ->where('runtime', '>', 120)
@@ -72,23 +35,11 @@ Route::get('/movies/runtime', function () {
     return view('thachthao_74', ['data' => $movies]);
 });
 
-<<<<<<< HEAD
 Route::get('/canada', 'App\Http\Controllers\MovieController@phimCanada');
 
 Route::get('/movies/action', [MovieController::class, 'actionMovies']);
 
 Route::get('/cau77', "App\Http\Controllers\MovieController@layThongTinPhim");
-=======
-// 7.5: Canada
-Route::get('/canada', [MovieController::class, 'phimCanada']);
-
-// 7.6: Action
-Route::get('/movies/action', [MovieController::class, 'actionMovies']);
-
-// 7.7: vote > 8 và vote_count > 10000
-Route::get('/cau77', [MovieController::class, 'layThongTinPhim']);
-
->>>>>>> 1e69824c31fcd6130eb5b9798a26f73d86391ef0
 
 Route::get('/top-budget', function () {
     $movies = DB::table('movie')
@@ -112,12 +63,4 @@ Route::get('/top10phimvote', function () {
         LIMIT 10
     ");
     return view('top10phimvote', ['data' => $data]);
-<<<<<<< HEAD
 });
-=======
-});
-
-Route::get('/trang1', [ViduLayoutController::class, 'trang1']);
-Route::get('/vidu2', [ViduLayoutController::class, 'sach']);
-Route::get('/sach/theloai/{id}', [ViduLayoutController::class, 'theloai']);
->>>>>>> 1e69824c31fcd6130eb5b9798a26f73d86391ef0
