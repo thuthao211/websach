1bd6956cc0f0e67206f4e4fe286395d85dc2d891
bachhop08 <030239230061@st.buh.edu.vn>
2026-03-24 09:57:59 +0700

    controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,9 +1,4 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> remotes/origin/thuthao
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -11,7 +6,6 @@ use Illuminate\Support\Facades\DB;
 
 class MovieController extends Controller
 {
-<<<<<<< HEAD
     public function actionMovies()
     {
 
@@ -24,7 +18,8 @@ class MovieController extends Controller
 
 
         return view('action', compact('movies'));
-=======
+    }
+}
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -35,10 +30,8 @@ class MovieController extends Controller {
             ->where('country_name', 'Canada')
             ->get();
         return view('canada', compact('ds_phim'));
->>>>>>> remotes/origin/nhuthao
     }
-}
-=======
+
     public function layThongTinPhim() {
     $movies =  DB::table("movie")
                     ->where('vote_average', '>', 8.0)
@@ -47,4 +40,3 @@ class MovieController extends Controller {
 
     return view('cau77', compact('movies')); }
 }
->>>>>>> remotes/origin/thuthao
