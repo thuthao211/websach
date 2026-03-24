a99cf474083f506d2eb7bace1b16c93d3aff5418
thuthao211 <030239230218@st.buh.edu.vn>
2026-03-24 09:37:40 +0700

    cập nhật

---
+++ resources/views/sach_layout.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  </head>
+  <body>
+    <header style='text-align:center'>
+      <img src="{{asset('hinh/banner_sach.jpg')}}" width="1000px">
+    </header>
+    <main style="width:1000px; margin:2px auto;">
+      <div class='row'>
+        <div class='col-3 pr-0'>
+          <nav class="navbar navbar-light">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+            <ul class="navbar-nav">
+        <li class="nav-item active">
+        <a class="nav-link" href="{{url('sach')}}">Trang chủ</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="{{url('sach/theloai/1')}}">Tiểu thuyết</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="{{url('sach/theloai/2')}}">Truyện ngắn - tản văn</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="{{url('sach/theloai/3')}}">Tác phẩm kinh điển</a>
+        </li>
+    </ul>
+          </nav>
+          <img src="{{asset('hinh/sidebar_1.jpg')}}" width="100%" class='mt-1'>
+          <img src="{{asset('hinh/sidebar_2.jpg')}}" width="100%" class='mt-1'>
+        </div>
+        <div class='col-9'>
+          @yield('content')
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,6 @@ return 'Thuthao211';
 });
 
 Route::get('/cau77', "App\Http\Controllers\MovieController@layThongTinPhim");
+Route::get('/trang1','App\Http\Controllers\ViduLayoutController@trang1');
+Route::get('/vidu2','App\Http\Controllers\ViduLayoutController@sach');
+Route::get('/sach/theloai/{id}','App\Http\Controllers\ViduLayoutController@theloai');
