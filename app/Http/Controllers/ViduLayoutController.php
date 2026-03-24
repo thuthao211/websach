068161eafa9f5c5b2801e7c10e76cfba02f477b8
ltphuonganh <030239230007@st.buh.edu.vn>
2026-03-24 10:29:00 +0700

    Tạo component Layout rỗng

---
+++ app/Http/Controllers/ViduLayoutController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; 
+
+class ViduLayoutController extends Controller
+{
+    public function trang1()
+    {
+        return view("vidulayout.trang1");
+    }
+
+    public function sach()
+    {
+        $data = DB::select("select * from sach order by gia_ban asc limit 0,8");
+        return view("vidusach.index", compact("data"));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ViduLayoutController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/thuthao',function(){
-return 'Thuthao211';
-});
 
+Route::get('/thuthao', function () {
+    return 'Thuthao211';
+});
 
 Route::get('/top-budget', function () {
     $movies = DB::table('movie')
@@ -19,3 +20,9 @@ Route::get('/top-budget', function () {
 
     return view('top_budget', ['movies' => $movies]);
 });
+
+Route::get('/trang1', [ViduLayoutController::class, 'trang1']);
+
+Route::get('/sach','App\Http\Controllers\ViduLayoutController@sach');
+
+Route::get('/sach/theloai/{id}','App\Http\Controllers\ViduLayoutController@theloai');
