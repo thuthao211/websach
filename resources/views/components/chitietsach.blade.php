712db8bcfbbda7ecd391884012bdb3ae5d1e3720
bachhop08 <030239230061@st.buh.edu.vn>
2026-03-24 14:33:18 +0700

    chi tiết sách

--- resources/views/components/chitietsach.blade.php
+++ resources/views/components/chitietsach.blade.php
@@ -1,36 +1,39 @@
-<head>
-    <style>
-        .grid-container{
-                display: grid;
-                grid-template-columns: 1fr 1fr;
-                grid-gap: 15px;
-            }
-    </style>
-</head>
-<body>
 @extends("components.sach_layout")
+
 @section("title","Chi tiết sách")
+
 @section("content")
-<div >
-  @foreach($data2 as $row)
-    <div >
-      <h3>{{$row->tieu_de}}</h3><br/>
-    <div class = "grid-container">
-        <div>
-            <img src="{{asset('hinh/image/'.$row->file_anh_bia)}}" width='200px' height='200px'><br>
+<style>
+    .grid-container{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 15px;
+    }
+</style>
+
+<div>
+@foreach($data2 as $row)
+    <div>
+        <h3>{{ $row->tieu_de }}</h3><br/>
+
+        <div class="grid-container">
+            <div>
+                <img src="{{ asset('hinh/image/'.$row->file_anh_bia) }}" width="200px" height="200px"><br>
+            </div>
+
+            <div>
+                Nhà cung cấp: <b>{{ $row->nha_cung_cap }}</b><br/>
+                Nhà xuất bản: <b>{{ $row->nha_xuat_ban }}</b><br/>
+                Tác giả: <b>{{ $row->tac_gia }}</b><br/>
+                Hình thức bìa: <b>{{ $row->hinh_thuc_bia }}</b><br/>
+            </div>
         </div>
+
         <div>
-            Nhà cung cấp:<b>{{$row->nha_cung_cap}}</b><br/>
-            Nhà xuất bản: <b>{{$row->nha_xuat_ban}}</b><br/>
-            Tác giả:<b>{{$row->tac_gia}}</b><br/>
-            Hình thức bìa:<b>{{$row->hinh_thuc_bia}}</b><br/>
-        <div>
+            <b>Mô tả:</b><br>
+            {{ $row->mo_ta }}
+        </div>
     </div>
-        <div>
-        <b> Mô tả: </b> <br>
-        {{$row->mo_ta}}
-    </div>
-  @endforeach
+@endforeach
 </div>
 @endsection
-</body>
